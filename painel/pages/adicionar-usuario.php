1cf7283fa19cc312b99be375ab6f22a62a9c76a6
Bruno <martinsgomes527@gmail.com>
2020-01-11 15:44:57 -0400

    Foi realizado uma estrutura de validação back-and no form na qual essa validação tem o objetivo de fazer co que o usuário não esqueça de preencher os campos do form, além de ter inserido um novo campo Select no form no qual é passado uma função foreach que chama uma variavél global da classe Painel.php

--- painel/pages/adicionar-usuario.php
+++ painel/pages/adicionar-usuario.php
@@ -1,5 +1,6 @@
 <?php
 	verificaPermissaoPagina(2);
+		
 
 ?>
 <div class="box-content">
@@ -10,30 +11,69 @@
 
 		<?php
 			if(isset($_POST['acao'])){
+				$login = $_POST['login'];
+				$nome = $_POST['nome'];
+				$senha = $_POST['password'];
+				$imagem = $_FILES['imagem'];
+				$cargo = $_POST['cargo'];
+				if ($login == '') {
+					Painel::alert('erro','Campo Login Obrigatório!');
+				}else if ($nome == '') {
+					Painel::alert('erro','Campo Nome Obrigatório!');
+				}else if ($senha == '') {
+					Painel::alert('erro','Campo Senha Obrigatório!');
+				}else if ($imagem['name'] == '') {
+					Painel::alert('erro','Campo Imagem Obrigatório!');
+				}else if ($cargo == '') {
+					Painel::alert('erro','Campo Cargo Obrigatório!');
+				}else{
+					//podemos cadastrar 
+					if ($cargo >= $_SESSION['cargo']) {
+						Painel::alert('erro','Você precisa escolher um cargo abaixo do seu');
+					}else if (Painel::imagemValida($imagem) == false) {
+						Painel::alert('erro','O formato da imagem é invalido ou maior que 300mb');
+					}else if (Painel::userExists($login)) {
+						Painel::alert('erro','O Login ja exite escolha outro !');
+					}else{
+						//apenas cadastrar no banco.
+						$usuario = new Usuario();
+						Painel::alert('sucesso','O cadastro do usuário '.$login.' foi realizado com sucesso!');
+		
+					}
+				}
+				
 
 			}
-			
-				
-		
+
 		?>
 
 		<div class="form-group">
 			<label>Login:</label>
-			<input type="text" name="login" required >
+			<input type="text" name="login"  >
 		</div><!--form-group-->
 
 		<div class="form-group">
 			<label>Nome:</label>
-			<input type="text" name="nome" required >
+			<input type="text" name="nome"  >
+		</div><!--form-group-->
+		<div class="form-group">
+			<label>Cargo:</label>
+			<select name="cargo">
+				<?php
+					foreach (Painel::$cargos as $key => $value) {
+						if($key < $_SESSION['cargo']) echo '<option value="'.$key.'">'.$value.'</option>';
+					}
+				?>
+			</select>
 		</div><!--form-group-->
 		<div class="form-group">
 			<label>Senha:</label>
-			<input type="password" name="password"  required>
+			<input type="password" name="password"  >
 		</div><!--form-group-->
 
 		<div class="form-group">
 			<label>Imagem</label>
-			<input type="file" name="imagem" required="">
+			<input type="file" name="imagem" >
 		
 		</div><!--form-group-->
 
