797a1d59d4dd03728169f98a895b9ad46cb6c62b
Bruno <martinsgomes527@gmail.com>
2020-01-14 15:29:13 -0400

    Foi criado duas funções na classe Usuario.php, uma função para verificar se o login já existe na hora de criar um novo cadastro, e foi criada uma função para cadastrar os novos usuários do painel

--- classes/Usuario.php
+++ classes/Usuario.php
@@ -15,6 +15,21 @@
 			}
 
 		}
+
+		public static function userExists($user){
+			$sql = Mysql::conectar()->prepare("SELECT `id` FROM `tb.admin_usuarios` WHERE user = ?");
+			$sql->execute(array($user));
+			if ($sql->rowCount() == 1) 
+				return true;
+			else
+				return false;
+			
+
+		}
+		public function cadastrarUsuario($user,$senha,$imagem,$nome,$cargo){
+			$sql =  Mysql::conectar()->prepare("INSERT INTO `tb.admin_usuarios` VALUES (null,?,?,?,?,?) ");
+			$sql->execute(array($user,$senha,$imagem,$nome,$cargo));
+		}
 	}
 
 
